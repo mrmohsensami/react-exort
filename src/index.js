e7e2e729fe7c4db8853e5ac184c0f27e8a2cbda4
MohsenSami <mohsensami88@gmail.com>
2023-02-26 12:51:58 +0330

    map loop

--- src/index.js
+++ src/index.js
@@ -2,21 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const cars = [
+    {
+        id: 1,
+        title: "This is title 1",
+        img: "https://picsum.photos/id/5/400",
+    },
+    {
+        id: 2,
+        title: "This is title 2",
+        img: "https://picsum.photos/id/15/400",
+    },
+];
+
 const Greeting = () => {
     return (
         <section className="d-flex">
-            <Image title="This is title 1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapien.</Image>
-            <Image title="This is title 2" />
+            {cars.map((car) => {
+                const { title, img } = car;
+                return <Image key={car.id} title={title} img={img} />;
+            })}
         </section>
     );
 };
 
 const Image = (props) => {
-    const { title, children } = props;
+    const { title, img, children } = props;
     return (
         <div style={{ flexDirection: "column" }} className="d-flex">
             <h3>{title}</h3>
-            <img className="image" src="https://picsum.photos/400" alt="xxx" />
+            <img className="image" src={img} alt="xxx" />
             <p>{children}</p>
         </div>
     );
