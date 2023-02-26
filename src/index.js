2be93c898ead331ea80ad8de771ae458bd20d6c9
MohsenSami <mohsensami88@gmail.com>
2023-02-26 11:56:40 +0330

    components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
-function Greeting() {
-    return <h1>Hello World</h1>;
-}
+const Greeting = () => {
+    return (
+        <section className="d-flex">
+            <Image />
+            <Image />
+            <Image />
+            <Image />
+            <Image />
+            <Image />
+        </section>
+    );
+};
+
+const Image = () => {
+    return (
+        <div style={{ flexDirection: "column" }} className="d-flex">
+            <Title />
+            <img className="image" src="https://picsum.photos/200/300" alt="xxx" />
+        </div>
+    );
+};
+
+const Title = () => {
+    return (
+        <>
+            <h3>This is title</h3>
+        </>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Greeting />);
