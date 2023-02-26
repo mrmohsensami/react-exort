b473d032e30cf2340e02f7d36d94bf6c41a1f518
MohsenSami <mohsensami88@gmail.com>
2023-02-26 13:03:52 +0330

    event

--- src/index.js
+++ src/index.js
@@ -27,12 +27,20 @@ const Greeting = () => {
 };
 
 const Image = (props) => {
+    const handleClick = () => {
+        console.log("clicked");
+    };
+    const consoleClick = (title) => {
+        console.log(title);
+    };
     const { title, img, children } = props;
     return (
         <div style={{ flexDirection: "column" }} className="d-flex">
             <h3>{title}</h3>
             <img className="image" src={img} alt="xxx" />
             <p>{children}</p>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => consoleClick(title)}>Console title</button>
         </div>
     );
 };
